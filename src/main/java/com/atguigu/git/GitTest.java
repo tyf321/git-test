4dfac5216861b4373917d7f247421dd560f75529
tyf <tyf@tyf.com>
2022-10-14 10:17:29 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello github");
         System.out.println("hello github3");
         System.out.println("hello");
+        System.out.println("hot-fix");
 
     }
 }
